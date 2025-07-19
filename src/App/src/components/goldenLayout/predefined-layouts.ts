991e105ba7a3ea90f221fddfda25036c6a1d39cd
Linc <762699299@qq.com>
2025-07-19 23:22:19 +0800

    enhance(app): layout

--- src/App/src/components/goldenLayout/predefined-layouts.ts
+++ src/App/src/components/goldenLayout/predefined-layouts.ts
@@ -15,30 +15,36 @@ const miniRowConfig: LayoutConfig = {
 				componentType: "White",
 				width: 0
 			} as ComponentItemConfig,
+			// 这样可以保证 white 变动时，这里组的宽度按比例调整
 			{
-				type: ItemType.component,
-				title: "MdEditor",
-				header: { show: "top", popout: false },
-				componentType: "MdEditor",
-				width: 50
-			} as ComponentItemConfig,
-			{
-				type: ItemType.stack,
+				type: ItemType.row,
 				content: [
 					{
 						type: ItemType.component,
-						title: "MdViewer",
+						title: "MdEditor",
 						header: { show: "top", popout: false },
-						componentType: "MdViewer",
+						componentType: "MdEditor",
 						width: 50
 					} as ComponentItemConfig,
 					{
-						type: ItemType.component,
-						title: "MdCodeMirror",
-						header: { show: "top", popout: false },
-						componentType: "MdCodeMirror",
-						width: 50
-					} as ComponentItemConfig
+						type: ItemType.stack,
+						content: [
+							{
+								type: ItemType.component,
+								title: "MdViewer",
+								header: { show: "top", popout: false },
+								componentType: "MdViewer",
+								width: 50
+							} as ComponentItemConfig,
+							{
+								type: ItemType.component,
+								title: "MdCodeMirror",
+								header: { show: "top", popout: false },
+								componentType: "MdCodeMirror",
+								width: 50
+							} as ComponentItemConfig
+						]
+					},
 				]
 			},
 			{
