67c60e1103398687ea2d786bfde80f95301813f7
Linc <762699299@qq.com>
2025-11-19 16:36:19 +0800

    feat: add list2echarts_radial

--- src/ABConverter/converter/abc_echarts.ts
+++ src/ABConverter/converter/abc_echarts.ts
@@ -1,3 +1,109 @@
-/** 仅负责转 js 文本，不负责图像部分 */
+/**
+ * 仅负责转为 echart 的 json/js 文本
+ * 
+ * 均为 md_str -> md_str, 不负责图表渲染部分
+ * 
+ * 需要结合 [any-block/obsidian-charts](https://github.com/any-block/obsidian-charts)
+ * 或 vuepress 生态系统中的 echarts 插件使用
+ */
 
+import {ABConvert_IOEnum, ABConvert, type ABConvert_SpecSimp} from "./ABConvert"
+import {ABConvertManager} from "../ABConvertManager"
+import {ABReg} from "../ABReg"
+import { type List_ListItem, ListProcess } from "./abc_list"
 
+const abc_list2echarts_radial = ABConvert.factory({
+  id: "list2echarts_radial",
+  name: "放射线树图",
+  match: "list2echarts_radial",
+  detail: "将列表转换为放射线树图",
+  process_param: ABConvert_IOEnum.text,
+  process_return: ABConvert_IOEnum.text,
+  process: (el, header, content: string): string=>{
+    let list_data: List_ListItem = ListProcess.list2data(content, false)
+    list_data = ListProcess.data2strict(list_data)
+    let radial_array: RadialNode[] = list2radial_array(list_data)
+    let radial_data: RadialNode = array2data(radial_array) as RadialNode
+
+    // json -> script string
+    const data_str = JSON.stringify(radial_data)
+    const script_str = `\
+const option = {
+  tooltip: {
+    trigger: 'item',
+    triggerOn: 'mousemove',
+  },
+  series: [
+    {
+      type: 'tree',
+      data: [${data_str}],
+      top: '18%',
+      bottom: '14%',
+      layout: 'radial',
+      symbol: 'emptyCircle',
+      symbolSize: 7,
+      initialTreeDepth: 3,
+      animationDurationUpdate: 750,
+      emphasis: {
+        focus: 'descendant',
+      },
+    },
+  ],
+}
+`
+    return script_str
+  }
+})
+
+// 树节点类型
+type RadialNode = {
+  name: string,
+  // value?: number, // 目前不支持这个属性
+  children?: RadialNode[]
+}
+function list2radial_array(data: List_ListItem): RadialNode[] {
+  let nodes: RadialNode[] = []        // 节点树
+  let prev_nodes: RadialNode[] = []   // 缓存每个level的最新节点
+
+  // 第一次变换，所有节点为 "key": {...} 形式
+  for (let index = 0; index<data.length; index++) {
+    // 当前节点
+    const item = data[index]
+    const current_key: string = item.content
+    const current_value: RadialNode = { name: current_key }
+    prev_nodes[item.level] = current_value
+
+    // 放入节点树的对应位置中
+    if (item.level>=1 && prev_nodes.hasOwnProperty(item.level-1)) { // 非根节点且有父节点
+      let lastItem = prev_nodes[item.level-1]
+      if (typeof lastItem != "object" || Array.isArray(lastItem)) {
+        console.error(`list数据不合规，父节点的value值不是{}类型`)
+        return nodes
+      }
+      if (!lastItem.hasOwnProperty("children")) {
+        lastItem.children = [current_value]
+      } else {
+        lastItem.children!.push(current_value)
+      }
+    } else if (item.level==0) { // 根节点
+      nodes.push(current_value)
+    } else { // 不合规
+      console.error(`list数据不合规，没有正规化. level:${item.level}, prev_nodes:${prev_nodes}`)
+      return nodes
+    }
+  }
+
+  return nodes
+}
+
+// 将data数组转对象，如果根节点只有一个则正常转，如果有多个根节点则自动加上名为root的根节点
+function array2data(array: object[]): object {
+  if (array.length==1) {
+    return array[0]
+  } else {
+    return {
+      name: "root",
+      children: array
+    }
+  }
+}
