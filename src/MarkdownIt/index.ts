5a5a7122f8b83d230aa20d5b72b22b994f27b0b0
Linc <762699299@qq.com>
2025-12-02 20:53:48 +0800

    release(MdIt): 3.3.5

--- src/MarkdownIt/index.ts
+++ src/MarkdownIt/index.ts
@@ -2,3 +2,4 @@
 export { jsdom_init } from './jsdom_init'
 export { ab_mdit, ab_mdit_client } from './index_mdit'
 export { abConvertEvent } from '../ABConverter/ABConvertEvent'
+export { ABConvertManager } from '../ABConverter/ABConvertManager' // for client
