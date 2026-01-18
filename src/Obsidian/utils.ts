a535b0bd4d9584c9e1dc6aab3b82e414145d97bf
Linc <762699299@qq.com>
2026-01-18 21:07:27 +0800

    feat(obsidian): command: rebuild view

--- src/Obsidian/utils.ts
+++ src/Obsidian/utils.ts
@@ -12,11 +12,24 @@ import {
 
 /** obsidian 命令管理 */
 export function registerCommands(plugin: Plugin) {
+  // 刷新视图
+  plugin.addCommand({
+    id: 'any-block-rebuild-view',
+    name: 'Refresh/rebuild current view',
+    // callback: () => {},
+    editorCallback: async (_editor, view) => {
+      if (!(view instanceof MarkdownView)) return
+      const leaf = view.leaf
+      if (!leaf) return
+      // @ts-expect-error 类型“WorkspaceLeaf”上不存在属性“rebuildView”
+      leaf.rebuildView()
+    }
+  })
 }
 
 /** obsidian 状态栏管理 (右下角区域) */
 export function registerStatus(plugin: Plugin) {
-  // 右下角状态栏 - 刷新按钮
+  // 刷新视图
   {
     const statusBtnContainer = plugin.addStatusBarItem();
     statusBtnContainer.addClass('mod-clickable');
@@ -29,8 +42,9 @@ export function registerStatus(plugin: Plugin) {
     statusBtn.setAttribute('aria-label', 'Rebuild View');
     statusBtn.setAttribute('data-tooltip-position', 'top');
     statusBtn.onclick = () => {
-      const leaf = plugin.app.workspace.getActiveViewOfType(MarkdownView)?.leaf; if (!leaf) { return }
-      // @ts-expect-error
+      const leaf = plugin.app.workspace.getActiveViewOfType(MarkdownView)?.leaf
+      if (!leaf) return
+      // @ts-expect-error 类型“WorkspaceLeaf”上不存在属性“rebuildView”
       leaf.rebuildView()
     }
   }
